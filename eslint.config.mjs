5efd9d8977dfbddf3c7972a7a5c8b59ada375cb9
rakibthedev <abdurrakib4896@gmail.com>
2025-08-08 02:39:22 +0600

    first deploy

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -2,6 +2,7 @@ import { dirname } from "path";
 import { fileURLToPath } from "url";
 import { FlatCompat } from "@eslint/eslintrc";
 
+
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
@@ -10,5 +11,22 @@ const compat = new FlatCompat({
 });
 
 const eslintConfig = [...compat.extends("next/core-web-vitals")];
+
+const compat = new FlatCompat({
+  baseDirectory: import.meta.dirname,
+})
+
+eslintConfig.push(
+  ...compat.config({
+    extends: ['next'],
+    rules: {
+      "react/react-in-jsx-scope": "off",
+      "react/no-unescaped-entities": "off",
+      "no-unused-vars": "warn",
+      "no-console": "warn",
+    },
+  })
+);
+
 
 export default eslintConfig;
